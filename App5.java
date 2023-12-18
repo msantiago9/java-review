a9d6e9a800d0e798ad811e42163d513bd8f59692
Marjon <marjonsantiago49@gmail.com>
2023-12-18 17:04:02 -0500

    Update App5.java

--- App5.java
+++ App5.java
@@ -3,7 +3,19 @@ public class App5 {
      * Challenge 5: Return Method
      * --
      * Task:
-     * Create a static method 
+     * Create a static method burnCalories().
+     * 
+     * burnCalories() accepts two variables as input:
+     * 1. String exercise
+     * 2. Integer minutes
+     * 
+     * exercise can be either biking, running, or swimming.
+     * 
+     * biking burns 1 calorie per minute.
+     * running burns 2 calories per minute.
+     * dancing burns 3 calories per minute.
+     * 
+     * Calculate the calories burned and return it as an int.
      */
     public static void main(String[] args) {
         // Your Code Below
